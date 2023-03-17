297b2ea9d26d30308f22c546c0d738fbdafdd5a9
Nikola <98809515+zxcghul@users.noreply.github.com>
2023-03-17 13:03:06 +0300

    Сделал 'адаптив' для главной страницы

--- src/js/app.js
+++ src/js/app.js
@@ -56,6 +56,30 @@ const sliderBasket = () => {
     });
 }
 
+const sliderNew = () => {
+    const slider = new Swiper('.slider-new', {
+        loop: true, 
+        speed: 500,
+        breakpoints: {
+            320: {
+                slidesPerView: 2,
+                // loopedSlides: 1,
+                // spaceBetween: 25
+              },
+            576: {
+                // loop: false,
+                slidesPerView: 3,
+                centeredSlides: true,
+                // slidesPerView: "auto",
+                // centeredSlides: true,
+                spaceBetween: 16
+            }
+        },
+        
+    });
+}
+
 slider();
 sliderBasket();
+sliderNew();
 
